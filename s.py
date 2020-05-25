5498857cce6e37bc924bb64f8762bb0d98d2ccd0
admin <admin@MacBook-Pro-admin.local>
2020-05-25 18:40:05 +0300

    chore: developer info dict

--- s.py
+++ s.py
@@ -1,7 +1,10 @@
+from pprint import pprint
+from typing import List, Dict
+
 from bs4 import BeautifulSoup
 import requests
 
-#if __name__ == '__main__':
+# if __name__ == '__main__':
 
 #    result = requests.get('https://dev.to/api/articles?top=50')
 
@@ -15,15 +18,21 @@ import requests
 
 #    print(soup)
 
-a = requests.get('https://dev.to/balvinder294')
 
-soup = BeautifulSoup(a.text, 'lxml')
+if __name__ == "__main__":
+    a = requests.get("https://dev.to/balvinder294")
 
-meta = soup.find_all('div', {
-    'class': ['key', 'value']
-})
+    soup = BeautifulSoup(a.text, "lxml")
 
+    meta = soup.find_all("div", {"class": ["key", "value"]})
+    keys: List[str] = []
+    values: List[str] = []
+    for tag in meta:
+        if tag.get("class") == ["key"]:
+            keys.append(tag.text.strip())
+        elif tag.get("class") == ["value"]:
+            values.append(tag.text.strip())
 
+    developer_info: Dict[str, str] = dict(zip(keys, values))
 
-print(meta)
-
+    pprint(developer_info)
