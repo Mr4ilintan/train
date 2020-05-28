1a26ee08ad262d49d366a75a14463429ef029aa1
admin <admin@MacBook-Pro-admin.local>
2020-05-28 22:29:51 +0300

    fixes

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from typing import List
 
-from devto_profile_parser import create_header, parse_devto_profiles
+from devto_profile_parser import create_header, parse_devto_profiles, Developer
 from popular_devto_parser import get_devto_profile_urls
 
 if __name__ == "__main__":
     create_header()
-    profile_urls: List[str] = get_devto_profile_urls()
-    parse_devto_profiles(profile_urls)
+    partial_profiles: List[Developer] = get_devto_profile_urls()
+    parse_devto_profiles(partial_profiles)
